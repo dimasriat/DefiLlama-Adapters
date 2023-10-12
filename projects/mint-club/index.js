dc198ef7875178ed883fe35c6adc5ac0fc949c46
Dimas <dimasriat@gmail.com>
2023-10-12 10:52:06 +0700

    feat(projects/factorDAO): add vault-genesis

--- projects/mint-club/index.js
+++ projects/mint-club/index.js
@@ -1,21 +1,41 @@
-const sdk = require('@defillama/sdk');
-const { transformBscAddress } = require('../helper/portedTokens');
-const MINT_TOKEN_CONTRACT = '0x1f3Af095CDa17d63cad238358837321e95FC5915';
-const MINT_CLUB_BOND_CONTRACT = '0x8BBac0C7583Cc146244a18863E708bFFbbF19975';
+const sdk = require("@defillama/sdk");
+const { transformBscAddress } = require("../helper/portedTokens");
+const MINT_TOKEN_CONTRACT = "0x1f3Af095CDa17d63cad238358837321e95FC5915";
+const MINT_CLUB_BOND_CONTRACT = "0x8BBac0C7583Cc146244a18863E708bFFbbF19975";
 
 async function tvl(timestamp, block, chainBlocks) {
   const balances = {};
   const transform = await transformBscAddress();
 
-  const collateralBalance = (await sdk.api.abi.call({
-    abi: 'erc20:balanceOf',
-    chain: 'bsc',
-    target: MINT_TOKEN_CONTRACT,
-    params: [MINT_CLUB_BOND_CONTRACT],
-    block: chainBlocks['bsc'],
-  })).output;
+  const collateralBalance = (
+    await sdk.api.abi.call({
+      abi: "erc20:balanceOf",
+      chain: "bsc",
+      target: MINT_TOKEN_CONTRACT,
+      params: [MINT_CLUB_BOND_CONTRACT],
+      block: chainBlocks["bsc"],
+    })
+  ).output;
 
-  sdk.util.sumSingleBalance(balances, transform(MINT_TOKEN_CONTRACT), collateralBalance)
+  console.log({
+    balances,
+    token: transform(MINT_TOKEN_CONTRACT),
+    collateralBalance,
+    block: chainBlocks["bsc"],
+  });
+
+  sdk.util.sumSingleBalance(
+    balances,
+    transform(MINT_TOKEN_CONTRACT),
+    collateralBalance
+  );
+
+  console.log({
+    balances,
+    token: transform(MINT_TOKEN_CONTRACT),
+    collateralBalance,
+    block: chainBlocks["bsc"],
+  });
 
   return balances;
 }
@@ -23,5 +43,5 @@ async function tvl(timestamp, block, chainBlocks) {
 module.exports = {
   bsc: {
     tvl,
-  }
+  },
 }; // node test.js projects/mint-club/index.js
